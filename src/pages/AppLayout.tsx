576aa21402f9ed3ac3ef13d7d46c67233395693d
Olabode Micheal AYOMIKUN <114963177+luli-tech@users.noreply.github.com>
2025-12-04 04:15:12 +0100

    logo

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -56,7 +56,7 @@ export function AppLayout() {
             <Link to="/dashboard" className="flex items-center gap-3">
               <div className="w-10 h-10 rounded-xl gradient-primary flex items-center justify-center">
                 {/* <Rocket className="w-5 h-5 text-white" /> */}
-                <img src="favico.ico"/>
+                <img src="favicon.ico"/>
                 <
               </div>
               <span className="text-xl font-bold">TaskPadi</span>
